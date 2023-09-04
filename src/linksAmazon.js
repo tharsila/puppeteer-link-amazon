b1adbec73795423fdff6f088bc5f96ace843ebc9
tharsila <tharsilapaula@gmail.com>
2023-09-04 17:06:19 -0300

    Corrige posicionamento do stats

--- src/linksAmazon.js
+++ src/linksAmazon.js
@@ -7,7 +7,6 @@ const fs = require("fs");
 
 const PCR = require("puppeteer-chromium-resolver");
 const options = {};
-const stats = await PCR(options);
 
 const url =
     "https://www.amazon.com.br/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com.br%2F%3Fref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=brflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0"
@@ -29,6 +28,7 @@ const linksAmazon = async (res) => {
     });
     
     try {
+        const stats = await PCR(options);
         const browser = await stats.puppeteer.launch({
             args:[...chromium.args, '--hide-scrollbars', '--disable-web-security'],
             executablePath: stats.executablePath,
