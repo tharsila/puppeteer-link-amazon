7c099b44cb50ae659359da6abd6cd1ea27a49763
tharsila <tharsilapaula@gmail.com>
2023-09-06 17:06:20 -0300

    Ajustando chamada do linksAmazon

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ const app = express();
 const port = 4000
 app.listen(port, () => {
     console.log("Rodando na porta: " + port);
+
+    // Chama a função linksAmazon automaticamente quando o servidor inicia
+    linksAmazon().then(() => {
+        console.log('Processo do Puppeteer concluído.');
+    }).catch(error => {
+        console.error('Erro ao executar o Puppeteer:', error);
+    });
 })
 
 app.get('/', (req, res) => {
@@ -15,6 +22,6 @@ app.get('/', (req, res) => {
 
 
 
-app.get('/api', (req, res) => {
+/* app.get('/api', (req, res) => {
     linksAmazon(res)
-})
+}) */
